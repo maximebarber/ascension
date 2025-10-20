1f61a3174acf805ebed7b1662d6fb89aa0e68cbf
maxime <maximebarber@gmail.com>
2025-10-20 11:08:41 +0200

    add GraphQL support and admin backend

--- api/src/Entity/Review.php
+++ api/src/Entity/Review.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -14,6 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ApiResource(
     operations: [
         new Get(),
+        new GetCollection(),
         new Post(),
         new Delete()
     ]
